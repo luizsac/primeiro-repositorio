aff7bafe709e6126e2c83db7a4c6d5d148ace670
luizsac <lu_que90@hotmail.com>
2021-12-21 09:31:04 -0300

    First commit

---
+++ PrimeiroRepositorio.java
@@ -0,0 +1,7 @@
+public class PrimeiroRepositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!\nEsta é a aplicação do meu primeiro repositório no GitHub");
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+## Primeiro repositório no GitHub.
+#### Primeiro repositório no GitHub contendo uma aplicação que imprime uma frase no console.
